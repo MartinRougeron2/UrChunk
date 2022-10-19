2bf1a16e01e4ae2e715e719047af5a181a7a0f09
martin <martin.rougeron@epitech.eu>
2022-10-19 06:33:41 +0100

    feat set token

--- off-chain/postandbuy-dapp/src/App.js
+++ off-chain/postandbuy-dapp/src/App.js
@@ -1,99 +1,237 @@
 import './App.css';
 import User from './contracts/User.json';
 import Post from './contracts/Post.json';
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
+import {withCookies, Cookies, useCookies} from 'react-cookie';
+// import axios
+import axios from 'axios';
+
+// get jwt token from backend and return it to the frontend
+function login(signature, address, message) {
+    return axios.post('http://localhost:8000/login', {
+        signature: signature,
+        address: address,
+        message: message
+    }).then(res => {
+        console.log(res);
+        return res;
+    }).catch(err => {
+        console.error(err);
+    });
+}
+
+function createUser(name, email, price) {
+    return axios.post('http://localhost:8000/create-user', {
+        name: name,
+        email: email,
+        price: price
+    }).then(res => {
+        console.log(res);
+        return res;
+    }).catch(err => {
+        console.error(err);
+    });
+}
+
+function createPost(title, content, price) {
+    return axios.post('http://localhost:8000/create-post', {
+        title: title,
+        content: content,
+        price: price
+    }).then(res => {
+        console.log(res);
+        return res;
+    }).catch(err => {
+        console.error(err);
+    });
+}
 
 // Language: javascript
 const App = () => {
+    axios.defaults.withCredentials = true;
+    //state variable to store user's public wallet
+    const [currentAccount, setCurrentAccount] = useState("");
 
-//state variable to store user's public wallet
-  const [currentAccount, setCurrentAccount] = useState("");
+    const [cookies, setCookie] = useCookies(['token'])
 
 // check wallet connection when the page loads
-  const checkIfWalletIsConnected = async () => {
+    const checkIfWalletIsConnected = async () => {
 
-    // access to window.ethereum
-    const {ethereum} = window;
+        // access to window.ethereum
+        const {ethereum} = window;
 
-    //check if user has metamask
-    if(!ethereum) {
-      alert("Make sure you have metamask");
-      return;
+        //check if user has metamask
+        if (!ethereum) {
+            alert("Make sure you have metamask");
+            return;
+        }
+
+        //get the wallet account
+        const accounts = await ethereum.request({method: 'eth_accounts'});
+
+        //get the first account
+        if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log("Found account:", account);
+
+            //set the account as a state
+            setCurrentAccount(account);
+        } else {
+            console.log("No account");
+        }
     }
-
-    //get the wallet account
-    const accounts = await ethereum.request({method: 'eth_accounts'});
-
-    //get the first account
-    if(accounts.length !== 0){
-      const account = accounts[0];
-      console.log("Found account:", account);
-
-      //set the account as a state
-      setCurrentAccount(account);
-    }
-    else{
-      console.log("No account");
-    }
-  }
 
 // connect to wallet
-  const connectWallet = async () => {
-    try {
-      const {ethereum} = window;
+    const connectWallet = async () => {
+        try {
+            const {ethereum} = window;
 
-      if(!ethereum) {
-        alert("Opps, looks like there is no wallet!");
-        return;
-      }
+            if (!ethereum) {
+                alert("Opps, looks like there is no wallet!");
+                return;
+            }
 
-      const currentNetwork = ethereum.networkVersion;
-      console.log("Current network", currentNetwork);
+            const currentNetwork = ethereum.networkVersion;
+            console.log("Current network", currentNetwork);
 
-      //check which network the wallet is connected on
-      if(currentNetwork !== 4){
-        // prompt user with a message to switch to network 4 which is the rinkeby network on metamask
-        alert("Opps, only works on Rinkeby! Please change your //network :)");
-        return;
-      }
+            //check which network the wallet is connected on
+            if (currentNetwork !== 4) {
+                // prompt user with a message to switch to network 4 which is the rinkeby network on metamask
+                alert("Opps, only works on Rinkeby! Please change your //network :)");
+                return;
+            }
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
-      setCurrentAccount(accounts[0]);
+            const accounts = await ethereum.request({method: "eth_requestAccounts"});
+            setCurrentAccount(accounts[0]);
 
+        } catch (error) {
+            console.log(error);
+        }
     }
-    catch( error){
-      console.log(error);
-    }
-  }
 
 
 //run function checkIfWalletIsConnected when the page loads
-  useEffect(()=> {
-    checkIfWalletIsConnected();
-  }, []);
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
 
 //connect to wallet
-  const walletNotConnected = () => (
-      <button onClick={connectWallet} className="connect-to-wallet-button">
-        Connect to Wallet
-      </button>
-  );
+    const walletNotConnected = () => (
+        <button onClick={connectWallet} className="connect-to-wallet-button">
+            Connect to Wallet
+        </button>
+    );
 
 //wallet connected
-  const walletConnected = () => (
-      <div>
-        <p>Connected to the wallet</p>
-      </div>
-  );
-
-  return (
-      <div className="App">
-        <div style={{display: 'flex', justifyContent:'center', height: '50px'}}>
-          {currentAccount === "" ? walletNotConnected()  : walletConnected()}
-          <br />
+    const walletConnected = () => (
+        <div>
+            <p>Connected to the wallet</p>
         </div>
-      </div>
-  );
+    );
+
+    // sign message
+    const signMessage = async () => {
+        const {ethereum} = window;
+        // sign a message to prove the user is the owner of the wallet
+        const message = "I am signing this message to prove ownership of my wallet";
+        const signature = await ethereum.request({
+            method: "personal_sign",
+            params: [message, currentAccount]
+        });
+        const res = await login(signature, currentAccount, message);
+        if (!res) {
+            console.error("Error response from backend is null");
+            return;
+        }
+        setCookie('token', res.data.token, {
+            path: '/',
+            expires: new Date(Date.now() + 3600000),
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+        });
+    }
+
+    const handleCreateUserSubmit = async (event) => {
+        event.preventDefault();
+        const name = event.target.elements.name.value;
+        const email = event.target.elements.email.value;
+        const price = event.target.elements.price.value;
+        const res = await createUser(name, email, price);
+        if (!res) {
+            console.error("Error response from backend is null");
+            return;
+        }
+        setCookie('user', res.data.userAddress, {
+            path: '/',
+            secure: true,
+            sameSite: "none",
+        })
+    }
+
+    const handleCreatePostSubmit = async (event) => {
+        event.preventDefault();
+        const title = event.target.elements.title.value;
+        const description = event.target.elements.content.value;
+        const price = event.target.elements.price.value;
+        const res = await createPost(title, description, price);
+        if (!res) {
+            console.error("Error response from backend is null");
+            return;
+        }
+        console.log(res);
+    }
+
+    return (
+        <div className="App">
+            <div style={{display: 'flex', justifyContent: 'center', height: '50px'}}>
+                {currentAccount === "" ? walletNotConnected() : walletConnected()}
+                <br/>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'center', height: '50px'}}>
+                <button
+                    onClick={signMessage}
+                    className="connect-to-wallet-button">
+                    Login
+                </button>
+                <br/>
+            </div>
+            <div>
+                <form onSubmit={handleCreateUserSubmit}>
+                    <label>
+                        Name:
+                        <input type="text" name="name"/>
+                    </label>
+                    <label>
+                        Email:
+                        <input type="text" name="email"/>
+                    </label>
+                    <label>
+                        Price:
+                        <input type="text" name="price"/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+            <div>
+                <form onSubmit={handleCreatePostSubmit}>
+                    <label>
+                        Title:
+                        <input type="text" name="title"/>
+                    </label>
+                    <label>
+                        Content:
+                        <input type="text" name="content"/>
+                    </label>
+                    <label>
+                        Price:
+                        <input type="text" name="price"/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- off-chain/server/app.js
+++ off-chain/server/app.js
@@ -1,6 +1,7 @@
 // init express app
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 // init web3
 const Web3 = require('web3');
@@ -8,18 +9,29 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 // init contract
 const contract = require('truffle-contract');
+
 const user_artifact = require('../postandbuy-dapp/src/contracts/User.json');
-const User = contract(user_artifact);
-User.setProvider(web3.currentProvider);
 const post_artifact = require('../postandbuy-dapp/src/contracts/Post.json');
+
+const User = contract(user_artifact);
 const Post = contract(post_artifact);
+
+User.setProvider(web3.currentProvider);
 Post.setProvider(web3.currentProvider);
 
 // set DB connection
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/postandbuy', {useNewUrlParser: true});
+// get environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://${username}:${password}@postandbuycluster.snuasop.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {useNewUrlParser: true});
 // set DB schema
 const Schema = mongoose.Schema;
+
 const userSchema = new Schema({
     address: String,
     name: String,
@@ -29,7 +41,6 @@ const userSchema = new Schema({
     owner: String,
     followers: Array,
 });
-const UserDB = mongoose.model('User', userSchema);
 const postSchema = new Schema({
     address: String,
     title: String,
@@ -40,24 +51,53 @@ const postSchema = new Schema({
     createdAt: Date,
     likes: Array,
 });
+
+const UserDB = mongoose.model('User', userSchema);
 const PostDB = mongoose.model('Post', postSchema);
 
+// jwt
+const jwt = require('jsonwebtoken');
 
-// log user using web3 and return jwt token and document with swagger
+// cookie parser
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser());
+
+// log user using web3 and return jwt token
 app.post('/login', async (req, res) => {
+    console.log(req.cookies.token);
+
+    const {signature, message} = req.body;
+
+    const address = req.body.address.toString().toLowerCase();
+    const recovered = web3.eth.accounts.recover(message, signature).toString().toLowerCase();
+
+    console.log('address', address);
+    console.log('recovered', recovered);
+
     // check signature
-    const signature = req.body.signature;
-    const address = req.body.address;
-    const message = req.body.message;
-    const recovered = web3.eth.accounts.recover(message, signature);
     if (recovered !== address) {
+        console.log('signature not match');
         res.status(401).send('Invalid signature');
         return;
     }
     // return jwt token
-    const token = jwt.sign({address: address}, 'secret', {expiresIn: '1h'});
-    res.status(200).send({token: token});
+    jwt.sign({address: address}, 'secret', {expiresIn: '1h'}, (err, token) => {
+        if (err) {
+            console.log('jwt error', err);
+            res.status(500).send('Error generating token');
+            return;
+        }
+        console.log('setting headers');
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        res.cookie('token', token, {
+            expires: new Date(Date.now() + 3_600_000),
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+        }).status(200).send('Login success');
+    });
 });
 
 // add post to database
@@ -101,21 +141,23 @@ app.post('/addPost', async (req, res) => {
 });
 
 // create user
-app.post('/createUser', async (req, res) => {
+app.post('/create-user', async (req, res) => {
     // check jwt token
-    const token = req.body.token;
+    const token = req.cookies.token;
     if (!token) {
         res.status(401).send('No token provided');
         return;
     }
+
     jwt.verify(token, 'secret', async (err, decoded) => {
         if (err) {
             res.status(401).send('Invalid token');
             return;
         }
         // create user
-        const tx = await User.new(req.body.name, req.body.content, req.body.price, {from: decoded.address});
-        const userAddress = tx.logs[0].address;
+        const tx = await User.new(req.body.name, req.body.email, req.body.price, {from: decoded.address});
+        console.log(tx.address);
+        const userAddress = tx.address;
         // add user to database
         const user = new UserDB({
             address: userAddress,
@@ -125,8 +167,59 @@ app.post('/createUser', async (req, res) => {
             owner: decoded.address,
         });
         await user.save();
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         // return user address
         res.status(200).send({userAddress: userAddress});
+    });
+});
+
+// create post
+app.post('/create-post', async (req, res) => {
+    // check jwt token
+    const token = req.cookies.token;
+    if (!token) {
+        res.status(401).send('No token provided');
+        return;
+    }
+    jwt.verify(token, 'secret', async (err, decoded) => {
+        if (err) {
+            res.status(401).send('Invalid token');
+            return;
+        }
+        // get user address from req
+        const address = req.cookies.user;
+        // get user contract from address in database
+        const userContractDB = await UserDB.findOne({address: address});
+        // get user contract
+        const userContract = await User.at(userContractDB.address);
+        // check if user is the same as the one in the token
+        const userAddress = await userContract.owner();
+        if (userAddress !== decoded.address) {
+            res.status(401).send('Invalid token');
+            return;
+        }
+        // create post
+        const tx = await userContract.createPost(req.body.name, req.body.content, req.body.price, {from: decoded.address});
+        const postAddress = tx.logs[0].address;
+        // add post to database
+        const post = new PostDB({
+            address: postAddress,
+            title: req.body.name,
+            content: req.body.content,
+            price: req.body.price,
+            owner: userContract.address,
+            author: userContract.address,
+            // createdAt: Date.now(),
+            createdAt: new Date.now(),
+            likes: [],
+        });
+        await post.save();
+        console.log('post', post);
+        // return post address
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        res.status(200).send({postAddress: postAddress});
     });
 });
 
@@ -402,7 +495,24 @@ app.post('/postChangePrice', async (req, res) => {
 });
 
 // get port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
+
+// cors middleware to allow cross origin requests
+const corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Methods": "GET, POST, DELETE",
+    "Access-Control-Allow-Credentials": 'http://localhost:3000',
+    "Access-Control-Max-Age": '600',
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    credentials: true
+
+};
+const cors = require('cors');
+app.use(cors(corsOptions));
+
+// start server
 
 // launch server
 app.listen(port, () => {
