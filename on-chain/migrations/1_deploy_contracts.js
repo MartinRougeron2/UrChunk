6efbacd36d6c43ed48ea65f604bcac884a36b744
martin <martin.rougeron@epitech.eu>
2022-10-21 16:34:35 +0100

    fix deployement

--- on-chain/migrations/1_deploy_contracts.js
+++ on-chain/migrations/1_deploy_contracts.js
@@ -1,7 +1,8 @@
 var User = artifacts.require("User");
 var Post = artifacts.require("Post");
 
-module.exports = function(deployer) {
-  deployer.deploy(User, "martin-ganache", "@", 1);
-  deployer.deploy(Post, "martin-ganache-post", "martin-ganache-content", 1, User.address);
+module.exports = function (deployer) {
+    deployer.deploy(User, "martin-ganache", "@", 1).then(function () {
+        return deployer.deploy(Post, "martin-ganache-post", "martin-ganache-content", 1, User.address);
+    });
 }
